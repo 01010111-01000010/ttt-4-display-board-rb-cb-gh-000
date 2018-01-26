5dbc88964b56cb57469bf3c1e157ae90285402d8
Wilson Blaikie <wilsonblaikie@gmail.com>
2018-01-26 12:48:57 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,9 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+def display_board (gameInputs)
+puts " #{gameInputs[0]} | #{gameInputs[1]} | #{gameInputs[2]} "
+puts "-----------"
+puts " #{gameInputs[3]} | #{gameInputs[4]} | #{gameInputs[5]} "
+puts "-----------"
+puts " #{gameInputs[6]} | #{gameInputs[7]} | #{gameInputs[8]} "
+end
